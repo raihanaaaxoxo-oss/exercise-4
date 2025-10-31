1682b55f7b8030d3d44b4ef69f5f2224e0083435
raihanaaaxoxo-oss <raihanaaaxoxo@gmail.com>
2025-10-31 09:33:01 +0200

    i created this code using conditional statements; particularly if-else statements, compare values, implementing multiple conditions.

---
+++ exercise_4_conditionals.js
@@ -0,0 +1,12 @@
+let score = 85
+const passingGrade = 60
+
+if (score >= passingGrade) {
+    console.log("the student has passed the exam.")
+} else {
+    console.log("the student has failed the exam.")
+}
+
+if (score == 100) {
+    console.log("you got a perfect score.")
+} 
